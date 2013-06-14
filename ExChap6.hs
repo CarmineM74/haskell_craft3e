0074315d380e125f87daac8d73b39c14ead59066
Carmine Moleti <carmine@moleti.it>
2013-06-14 14:44:06 +0200

    Ex 6.23

--- ExChap6.hs
+++ ExChap6.hs
@@ -1,6 +1,6 @@
 module ExChap6 where
 import Prelude
-import Chapter6
+import Pictures
 
 -- 6.1
 -- 6.2
@@ -22,10 +22,10 @@ printPicture          :: Picture -> IO ()
 printPicture p = putStr (concat [l ++ "\n"| l <- p])
 
 -- 6.8
-width                 :: Picture -> Int
-width p 
-  | p == [] = 0 
-  | otherwise = length (p !! 0)
+--width                 :: Picture -> Int
+--width p 
+--  | p == [] = 0 
+--  | otherwise = length (p !! 0)
 
 pickColumn            :: Picture -> Int ->[Char] 
 pickColumn p col = [l !! col | l <- p]
@@ -48,3 +48,45 @@ scale p factor = [ scaleLine (p !! rownum) factor | rownum <- rownums]
   where
     height_p = (height p) - 1
     rownums = concat [replicate factor x | x <- [0..height_p]]
+
+-- 6.17
+
+pad                   :: Picture -> Int -> Picture
+pad p p_width = [l ++ (replicate padding '.') | l <- p]
+  where
+    padding = p_width - (width p)
+
+above'                :: Picture -> Picture -> Picture
+above' p1 p2 = p1_padded ++ p2_padded
+  where
+    max_width = max (width p1) (width p2)
+    p1_padded = pad p1 max_width
+    p2_padded = pad p2 max_width
+
+
+-- 6.23
+type RlePicture = [[(Int,Char)]]
+
+howManyConsecutive    :: Char -> [Char] -> Int
+howManyConsecutive c xs
+  | xs == [] = 0
+  | c == (head xs) = 1 + (howManyConsecutive c (tail xs))
+  | otherwise = 0 
+
+rlePackRow            :: [Char] -> [(Int,Char)]
+rlePackRow xs
+  | xs == [] = []
+  | otherwise = [(consecutives,h)] ++ (rlePackRow rest)
+    where
+      (h,t) = (head xs, tail xs)
+      consecutives = (howManyConsecutive h t) + 1
+      rest = drop consecutives xs 
+
+rlePack               :: Picture -> RlePicture
+rlePack p = [rlePackRow l | l <- p]
+
+rleUnpackRow          :: [(Int,Char)] -> [Char]
+rleUnpackRow xs = concat [ replicate cnt pixel | (cnt,pixel) <- xs]
+
+rleUnpack             :: RlePicture -> Picture
+rleUnpack p = [ rleUnpackRow l | l <- p]

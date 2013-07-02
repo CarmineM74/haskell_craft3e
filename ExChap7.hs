69a5ddcb991e36acf672da6503a2343dfcd3b7a4
Carmine Moleti <carmine.moleti@gmail.com>
2013-07-02 17:12:35 +0200

    Ex -> 7.11

--- ExChap7.hs
+++ ExChap7.hs
@@ -1,6 +1,6 @@
 module ExChap7 where
 import Data.Char
-import Prelude hiding (product, and, or)
+import Prelude hiding (product, and, or, reverse, unzip)
 
 -- 7.1
 digits                  :: String -> String
@@ -41,3 +41,31 @@ and (x:xs) = x && (and xs)
 or                      :: [Bool] -> Bool
 or [] = False
 or (x:xs) = x || (or xs)
+
+-- 7.8
+elemNum                 :: Integer -> [Integer] -> Integer
+elemNum _ [] = 0
+elemNum x (y:ys)
+  | x == y = 1 + elemNum x ys
+  | otherwise = elemNum x ys
+
+elemNum'                :: Integer -> [Integer] -> Integer
+elemNum' x ys = sum [1 | y <- ys, x == y]
+
+-- 7.9
+unique                  :: [Integer] -> [Integer]
+unique [] = []
+unique (y:ys)
+  | elemNum y ys == 0 = y : unique ys
+  | otherwise = unique ys
+
+-- 7.11 primitive recursive definitions of reverse and unzip
+reverse                 :: [a] -> [a]
+reverse [] = []
+reverse (x:xs) = reverse xs ++ [x]
+
+unzip                   :: [(a,b)] -> ([a],[b])
+unzip [] = ([],[])
+unzip (x:xs) = ([fst x] ++ (fst step),[snd x] ++ (snd step)) 
+  where
+    step = unzip xs
